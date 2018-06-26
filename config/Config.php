ae50217d13a875e09db7382a51604c2322ba6263
Aakash Technicals <1akash2@gmail.com>
2018-06-26 17:05:06 +0530

    Framework
    Adding config.php

---
+++ config/Config.php
@@ -0,0 +1,20 @@
+<?php 
+/* 
+Created By : Aakash C.Aware
+
+Website : www.akashaware.tech
+
+Date : 26-06-2018
+
+File Name : Config.php
+
+Description : Website configuration
+*/
+
+$config['base_path'] = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+         
+return $config;
+
+
+
+?>
